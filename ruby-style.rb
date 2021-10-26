0a2d0a7f3b418002e2c54cf3ab813be388211835
roorooro1 <roorooro9933@gmail.com>
2021-10-26 18:33:57 +0900

    ruby style guide

---
+++ ruby-style.rb
@@ -0,0 +1,11 @@
+# 기본적으로 snake style을 쓰지만.
+def my_name_is
+    put "my_name_is method"
+end
+
+# Class는 CamelCase로 작성한다는 특징이 있다.
+class SomeClass
+end
+
+# ruby style guide를 구글에 치면은 더 많은 정보를 얻을 수 있다.
+
