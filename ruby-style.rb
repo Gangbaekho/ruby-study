442308076d8d90fd40e5d7e3b3e3354410e8f9bd
roorooro1 <roorooro9933@gmail.com>
2021-10-26 18:35:47 +0900

    comment about indentation

--- ruby-style.rb
+++ ruby-style.rb
@@ -1,4 +1,8 @@
 # 기본적으로 snake style을 쓰지만.
+# Indentation은 tab을 쓰면 되는데
+# 기본적으로는 2 space bar를 사용한다네.
+# Indentation으로 구분하는것은 Python이랑 비슷한 것 같고
+# 익숙하지 않아서 그런지 딱히 내 스타일은 아닌 듯 하다.
 def my_name_is
     put "my_name_is method"
 end
